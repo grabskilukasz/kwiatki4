b3f146dfdbae872a33da038f2f481b6a8d759cc1
lukaszgrabski <luksz1886@gmail.com>
2023-11-18 11:53:34 +0100

    changes_1

--- main.py
+++ main.py
@@ -9,26 +9,7 @@ from pydantic import BaseModel
 import sqlite3
 
 app = FastAPI()
-
-df = pd.read_csv('Iris.csv')
-X = df.drop(['Id', 'Species'], axis=1)
-y = df['Species']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
-
 knc = KNeighborsClassifier(n_neighbors=3)
-knc.fit(X_train,y_train)
-y_pred = knc.predict(X_test)
-acc_knn = metrics.accuracy_score(y_pred,y_test)
-print('The accuracy of the KNN is', acc_knn)
-print(X_test[:1])
-
-with open('knn_model.pkl', 'wb') as file:
-    pickle.dump(knc, file)
 
 
 class IrisItem(BaseModel):
@@ -36,6 +17,31 @@ class IrisItem(BaseModel):
     sepal_width: float
     petal_length: float
     petal_width: float
+
+
+@app.get("/health")
+def root():
+    return {"Hello": "World"}
+
+
+@app.get("/train")
+def train():
+    df = pd.read_csv("Iris.csv")
+    X = df.drop(["Id", "Species"], axis=1)
+    y = df["Species"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
+
+    knc.fit(X_train, y_train)
+    y_pred = knc.predict(X_test)
+    acc_knn = metrics.accuracy_score(y_pred, y_test)
+    print("The accuracy of the KNN is", acc_knn)
+
+    with open("knn_model.pkl", "wb") as file:
+        pickle.dump(knc, file)
+
+    return f" The accuracy of the KNN is {acc_knn}"
+
 
 @app.post("/predict")
 def predict_species(item: IrisItem):
@@ -53,32 +59,31 @@ def predict_species(item: IrisItem):
         # W przypadku błędu zwróć informację o błędzie
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
-@app.get("/1")
-def root():
-    return {"Hello": "World"}
-
 
 @app.post("/respond")
 def respond(number: int):
     response_message = f"Received number: {number}."
 
-    conn = sqlite3.connect('example.db')
+    conn = sqlite3.connect("example.db")
     cursor = conn.cursor()
-    cursor.execute('''
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS numbers (
-            id INTEGER PRIMARY KEY,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             number INTEGER NOT NULL
         )
-    ''')
+    """
+    )
     cursor.execute("INSERT INTO numbers (number) VALUES (?)", (number,))
     conn.commit()
     conn.close()
 
     return {"response": response_message}
 
+
 @app.get("/read")
 def respond():
-    conn = sqlite3.connect('example.db')
+    conn = sqlite3.connect("example.db")
     cursor = conn.cursor()
 
     cursor.execute("SELECT * FROM numbers")
